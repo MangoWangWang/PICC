97c43565860e95fe35b707a512c66022ee12b0c1
mangowangwang <6579739qwe>
2017-09-27 11:43:43 +0800

    偷偷加了注释

--- app/src/main/java/com/piccjm/picc/aty/LoginActivity.java
+++ app/src/main/java/com/piccjm/picc/aty/LoginActivity.java
@@ -2,14 +2,17 @@ package com.piccjm.picc.aty;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.piccjm.picc.R;
 
+// 偷偷加个注释
 public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Toast.makeText(this, "haha", Toast.LENGTH_SHORT).show();
     }
 }
